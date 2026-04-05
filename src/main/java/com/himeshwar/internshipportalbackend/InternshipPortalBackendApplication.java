c8a0969fb8e954439bc098406f518a4d441ef4e1
Himeshwar Reddy Manyam <hrm22@student.le.ac.uk>
2026-04-05 03:51:12 +0100

    Initial project setup

---
+++ src/main/java/com/himeshwar/internshipportalbackend/InternshipPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.himeshwar.internshipportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipPortalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipPortalBackendApplication.class, args);
+    }
+
+}
